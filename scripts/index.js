ac57be98eb87f454478fec2821e0187a7d70bde3
Obradovic Nikola <obradovicnikola009@gmail.com>
2020-05-31 18:43:09 +0200

    main functionality done

--- scripts/index.js
+++ scripts/index.js
@@ -1,89 +1,225 @@
-const container = document.querySelector('.container');
-var inputValue = document.querySelector('.input');
-const add = document.querySelector('.add');
+let myInput = document.getElementById("myInput");
+let errorMsg = document.getElementById("error-msg");
+let list = document.getElementById("meals");
 
+const multiSpacePattern = /\s\s+/g;
 
-let meals = []
-if(localStorage.getItem("meals") == undefined){
+function createElementFromString(value, likes) {
+  const template = `
+  <li class="item">
+    <div class="left">
+     <input type="checkbox" />
+      <p class="name">${value}</p>
+    </div>
+    <div class="right">
+      <span class="counter">${likes}</span>
+      <span class="like">
+        <i class="fas fa-thumbs-up"></i>
+      </span>
+      <span class="dislike">
+        <i class="fas fa-thumbs-down"></i>
+      </span>
+    </div>
+  </li>`;
+
+  let div = document.createElement("div");
+  div.innerHTML = template.trim();
+
+  return div.firstChild;
+}
+
+let meals = [{}];
+if (localStorage.getItem("meals") == undefined) {
+  meals = [
+    { name: "Spageti", likes: 3, checked: false },
+    { name: "Sarma", likes: 5, checked: false },
+    { name: "Rostilj", likes: 7, checked: false },
+  ];
   localStorage.setItem("meals", JSON.stringify(meals));
 } else {
   const mealsJSON = localStorage.getItem("meals");
   meals = JSON.parse(mealsJSON);
 }
+function initializeListElements() {
+  for (let i = 0; i < meals.length; i++) {
+    meals[i].checked = false;
+    let newItem = createElementFromString(meals[i].name, meals[i].likes);
+    let title = newItem.getElementsByClassName("name")[0];
 
-// add.addEventListener('click', check);
-// window.addEventListener('keydown', (e) => {
-// 	if(e.which == 13){
-// 		check();
-// 	}
-// })
+    let thumbUp = newItem.getElementsByClassName("like")[0];
+    thumbUp.onclick = function like() {
+      let counter = newItem.getElementsByClassName("counter")[0];
+      let likes = counter.innerHTML;
+      likes++;
+      counter.innerHTML = likes;
 
-// function check(){
-// 	if(inputValue.value != ""){
-// 		new item(inputValue.value);
-//         todos.push(inputValue.value);
-//         window.localStorage.setItem("todos", JSON.stringify(todos));
-// 		inputValue.value = "";
-// 	}
-// }
+      for (let j = 0; j < meals.length; j++) {
+        if (meals[j].name == title.innerHTML) {
+          meals[j].likes = likes;
+          localStorage.setItem("meals", JSON.stringify(meals));
+          return;
+        }
+      }
+    };
 
+    let thumbDown = newItem.getElementsByClassName("dislike")[0];
+    thumbDown.onclick = function like() {
+      let counter = newItem.getElementsByClassName("counter")[0];
+      let likes = counter.innerHTML;
+      likes--;
+      counter.innerHTML = likes;
 
-// for (var v = 0 ; v < todos.length ; v++){
-//     new item(todos[v]);
-// }
+      for (let j = 0; j < meals.length; j++) {
+        if (meals[j].name == title.innerHTML) {
+          meals[j].likes = likes;
+          localStorage.setItem("meals", JSON.stringify(meals));
+          return;
+        }
+      }
+    };
 
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LIli");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
+    let inputCheks = newItem.getElementsByTagName("input");
+    for (let i = 0; i < inputCheks.length; i++) {
+      inputCheks[i].addEventListener("click", function hey() {
+        if (title.style.textDecoration == "line-through") {
+          title.style.textDecoration = "none";
+          for (let j = 0; j < meals.length; j++) {
+            if (meals[j].name == title.innerHTML) {
+              meals[j].checked = false;
+              // ovo se ne pise u local storage!
+              return;
+            }
+          }
+        } else {
+          title.style.textDecoration = "line-through";
+          for (let j = 0; j < meals.length; j++) {
+            if (meals[j].name == title.innerHTML) {
+              meals[j].checked = true;
+              // ovo se ne pise u local storage!
+              return;
+            }
+          }
+        }
+      });
+    }
 
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
+    list.prepend(newItem);
   }
 }
 
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
+initializeListElements();
+
+function resetError() {
+  errorMsg.className = "is-hidden";
+}
+myInput.addEventListener("focus", resetError);
+
+function parseInput() {
+  const newMeal = myInput.value.replace(multiSpacePattern, " ").trim();
+
+  if (newMeal == undefined || newMeal == "") {
+    myInput.value = "";
+    errorMsg.className = "";
+    return;
   }
-}, false);
+  resetError();
+  addItem();
+}
 
-// Create a new list item when clicking on the "Add" button
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
+function addItem() {
+  let newItem = createElementFromString(myInput.value, 0);
+  let title = newItem.getElementsByClassName("name")[0];
+  meals.push({ name: myInput.value, likes: 0, checked: false });
+  localStorage.setItem("meals", JSON.stringify(meals));
+
+  let thumbUp = newItem.getElementsByClassName("like")[0];
+  thumbUp.onclick = function like() {
+    let counter = newItem.getElementsByClassName("counter")[0];
+    let likes = counter.innerHTML;
+    likes++;
+    counter.innerHTML = likes;
+
+    for (let j = 0; j < meals.length; j++) {
+      if (meals[j].name == title.innerHTML) {
+        meals[j].likes = likes;
+        localStorage.setItem("meals", JSON.stringify(meals));
+        return;
+      }
+    }
+  };
+
+  let thumbDown = newItem.getElementsByClassName("dislike")[0];
+  thumbDown.onclick = function like() {
+    let counter = newItem.getElementsByClassName("counter")[0];
+    let likes = counter.innerHTML;
+    likes--;
+    counter.innerHTML = likes;
+
+    for (let j = 0; j < meals.length; j++) {
+      if (meals[j].name == title.innerHTML) {
+        meals[j].likes = likes;
+        localStorage.setItem("meals", JSON.stringify(meals));
+        return;
+      }
+    }
+  };
+
+  let inputCheks = newItem.getElementsByTagName("input");
+    for (let i = 0; i < inputCheks.length; i++) {
+      inputCheks[i].addEventListener("click", function hey() {
+        if (title.style.textDecoration == "line-through") {
+          title.style.textDecoration = "none";
+          for (let j = 0; j < meals.length; j++) {
+            if (meals[j].name == title.innerHTML) {
+              meals[j].checked = false;
+              // ovo se ne pise u local storage!
+              return;
+            }
+          }
+        } else {
+          title.style.textDecoration = "line-through";
+          for (let j = 0; j < meals.length; j++) {
+            if (meals[j].name == title.innerHTML) {
+              meals[j].checked = true;
+              // ovo se ne pise u local storage!
+              return;
+            }
+          }
+        }
+      });
+    }
+
+  list.prepend(newItem);
+  myInput.value = "";
+}
+
+myInput.addEventListener("keydown", (e) => {
+  resetError(); // when typing a new value, hide error msg
+  if (e.which == 13) {
+    parseInput();
   }
-  document.getElementById("myInput").value = "";
+});
 
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
+let removeSelected = document.getElementById("removeSelected");
+let removeAll = document.getElementById("removeAll");
 
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
+removeSelected.addEventListener("click", function removeSelected() {
+  for (let i = 0; i < meals.length; i++) {
+    if (meals[i].checked) {
+      meals.splice(i, 1);
+      i--;
     }
   }
-}
+  localStorage.setItem("meals", JSON.stringify(meals));
+
+  list.innerHTML = "";
+  initializeListElements();
+});
+
+removeAll.addEventListener("click", function removeAll() {
+  meals = [];
+
+  localStorage.setItem("meals", JSON.stringify(meals));
+  list.innerHTML = "";
+  initializeListElements();
+});
